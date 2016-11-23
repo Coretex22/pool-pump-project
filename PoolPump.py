a179b7d6e57da80d385b05de333f64c01d6d0fd8
Coretex22 <lukus22@gmail.com>
2016-11-23 14:42:46 +1030

    1.01
    added exception handler for pump timer input

--- PoolPump.py
+++ PoolPump.py
@@ -17,8 +17,6 @@ while pumpstate == 'off' or 'on':
 
     firstmenuselection = raw_input()
 
-
-
     if firstmenuselection == '1':
         print ''
         print 'Please enter a new pump state (on or off): '
@@ -43,13 +41,20 @@ while pumpstate == 'off' or 'on':
         print 'Please enter the timer interval in minutes'
         print '4 hours is 240 minutes'
         print ''
-        pumptimer = input()
-        pumpstate = 'on'
-        print 'The pump has been turned on for the specified amount of time, and will be turned off automatically'
-        time.sleep (pumptimer * 60)
-        pumpstate = 'off'
-        print ''
-        print 'Timer has expired, and the pump has been turned off'
+
+        try:
+            pumptimer = int(input())
+
+            pumpstate = 'on'
+            print 'The pump has been turned on for the specified amount of time, and will be turned off automatically'
+            time.sleep (pumptimer * 60)
+            pumpstate = 'off'
+            print ''
+            print 'Timer has expired, and the pump has been turned off'
+            print ''
+
+        except NameError:
+            print 'Please enter a valid integer'
 
     else: print 'Please make a valid selection'
     print ''
