032ad47c45f9351fcf56318148665fe6a5ac2853
Luke Davis <lukus22@gmail.com>
2016-11-25 10:52:13 +1030

    Moved comment line to be relevant.

--- PoolPumpRPi.py
+++ PoolPumpRPi.py
@@ -1,6 +1,5 @@
 #This program is written to remotely control a pool pump using a RaspberryPi's GPIO Pins.
 
-#This is because I don't know how to list shit properly yet:
 import time
 import RPi.GPIO as GPIO
 #This configures the GPIO pin numbering scheme
@@ -13,7 +12,7 @@ GPIO.output(11, GPIO.LOW)
 pumpstate = 'off'
 
 while pumpstate == 'off' or 'on':
-
+#This is because I don't know how to list shit properly yet:
     print ''
     print 'Please choose an option:'
     print ''
